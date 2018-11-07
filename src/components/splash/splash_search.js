bb2ff33cecf48a947dec347c3c7cb8dfa7cab3ca
Tyler <tylerthomasallen@gmail.com>
2018-11-07 11:30:09 -0800

    Add searchbar to splash page and example destinations

--- src/components/splash/splash_search.js
+++ src/components/splash/splash_search.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
     container: {
@@ -12,11 +13,77 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 5,
         marginBottom: 5
+    },
+
+    inputContainer: {
+        fontSize: 18,
+        borderColor: '#BDBDBD',
+        borderWidth: .5,
+        borderRadius: 5,
+        padding: 10,
+        marginTop: 10,
+        flexDirection: 'row',
+        shadowColor: '#BDBDBD',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1,
+        shadowRadius: 2
+    },
+
+    searchButton: {
+        margin: 5,
+        fontSize: 28,
+        color: 'blue',
+        fontWeight: '900'
+
+    },
+
+    input: {
+        shadowOpacity: 0,
+        shadowColor: 'white',
+        margin: 5
+
+    },
+
+    shadow: {
+        backgroundColor: '#BDBDBD',
+        padding: .5,
+        width: '98%',
+        alignSelf: 'center',
+        borderRadius: 5,
+        opacity: .3
+    },
+
+    addressContainer: {
+        margin: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    starContainer: {
+        backgroundColor: 'pink',
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    star: {
+        color: 'white',
+        fontSize: 24
+    },
+
+    address: {
+        flexDirection: 'column',
     }
+
+
+    
 });
 
 
 class SplashSearch extends React.Component {
+
 
     render() {
 
@@ -24,13 +91,34 @@ class SplashSearch extends React.Component {
             <View style={
                 styles.container
             }>
-            <Text>
-                Hi there
-            </Text>
+                <Text style={{fontSize: 18}}>
+                    Hi there
+                </Text>
 
-            <Text style={styles.title}>
-                Where are you going?            
-            </Text>
+                <Text style={styles.title}>
+                    Where are you going?            
+                </Text>
+
+                <View style={styles.inputContainer}>
+                    <Ionicons name="ios-search" style={styles.searchButton}/>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Search destination"
+                    >  
+                    </TextInput>
+                </View>
+            
+                <View style={styles.shadow} />
+
+                <View style={styles.addressContainer}>
+                    <View style={styles.starContainer}>
+                        <Ionicons name="ios-star" style={styles.star}/>
+                    </View>
+                    <View styles={styles.address}>
+                        <Text>Ginnas Place</Text>
+                        <Text>2268 Jackson St, San Francisco</Text>
+                    </View>
+                </View>
 
             </View>
         );
