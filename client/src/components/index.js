0cefaa5c43a07ec7685f975d11115c2a6eb3fa2e
Tyler <tylerthomasallen@gmail.com>
2019-02-19 15:36:57 -0800

    Refactor Index comp

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,22 +3,15 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './home';
 import RouteSearch from './route';
 
-const AppNavigator = createStackNavigator({
-    Home: HomeScreen,
-    Route: RouteSearch
-    },
-    {
-        initialRouteName: "Home"
-    }
+const AppNavigator = createStackNavigator({ Home: HomeScreen, Route: RouteSearch }, { initialRouteName: "Home" });
 
-);
 
 const AppContainer = createAppContainer(AppNavigator);
 
-export default class Index extends React.Component {
-    render() {
-        return (
-            <AppContainer />
-        )
-    }
+const Index = () => {
+  return(
+    <AppContainer />
+  )
 }
+
+export default Index;
