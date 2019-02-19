546685da658c2ba144221ef73058970c469b9444
Tyler <tylerthomasallen@gmail.com>
2019-02-19 14:38:33 -0800

    Convert App.js into a functional component

--- client/App.js
+++ client/App.js
@@ -3,25 +3,12 @@ import { Provider } from 'react-redux';
 import store from './src/store/store';
 import AppContainer from './src/components';
 
-
-export default class App extends Component {
-
-  test() {
-    fetch('http://172.68.132.93:5000/test')
-    .then(res => {
-      console.log(res);
-      debugger;
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-          <AppContainer />
-      </Provider>
-    );
-  }
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  );
 }
+
+export default App;
